55d895e1c2dd4065333645954133fc7311609cba
Chris Skorlinski <chrissk@Microsoft.com>
2020-12-06 14:48:13 -0500

    Update MS_DistributorMetadata.sql
    Added comments on MSrepl_commands and MSrepl_transactions queries pulling data by TOP xact_seqno (LSN), however table is indexes first by publication ID resulting in table (range scan) for large tables.  This may have performance impact on larger databases.

--- MS_DistributorMetadata.sql
+++ MS_DistributorMetadata.sql
@@ -1,10 +1,23 @@
 --Distribution Database Replication Metadata Collection Script
---Execute On Distributor: modify backup location below as needed
---Assumes Distribution database names is 'distribution'
+-- Execute On Distributor
+--Change as required:
+--    Assumes Distribution database names is 'distribution'
+--    Modify backup location below as needed
+
 CREATE DATABASE MS_DistBackup
 Go
 USE MS_DistBackup
 go
+-- For distribution DB +250gb range consider commenting next 4 select statements
+SELECT Top 100 * INTO MSrepl_commands_OLDEST from Distribution..MSrepl_commands with (nolock)
+	order by xact_seqno asc
+SELECT Top 100 * INTO MSrepl_commands_NEWEST from Distribution..MSrepl_commands with (nolock)
+	order by xact_seqno desc
+SELECT top 100 * INTO MSrepl_transactions_OLDEST from Distribution..MSrepl_transactions with (nolock)
+	order by xact_seqno asc
+SELECT top 100 * INTO MSrepl_transactions_NEWEST from Distribution..MSrepl_transactions with (nolock)
+	order by xact_seqno desc
+	
 SELECT * INTO MSarticles from Distribution..MSarticles with (nolock)
 SELECT * INTO MScached_peer_lsns from Distribution..MScached_peer_lsns with (nolock)
 SELECT * INTO MSdistribution_agents from Distribution..MSdistribution_agents with (nolock)
@@ -24,17 +37,9 @@ SELECT * INTO MSpublisher_databases from Distribution..MSpublisher_databases wit
 SELECT * INTO MSqreader_agents from Distribution..MSqreader_agents with (nolock)
 SELECT * INTO MSqreader_history from Distribution..MSqreader_history with (nolock)
 SELECT * INTO MSrepl_backup_lsns from Distribution..MSrepl_backup_lsns with (nolock)
-SELECT Top 100 * INTO MSrepl_commands_OLDEST from Distribution..MSrepl_commands with (nolock)
-	order by xact_seqno asc
-SELECT Top 100 * INTO MSrepl_commands_NEWEST from Distribution..MSrepl_commands with (nolock)
-	order by xact_seqno desc
 SELECT * INTO MSrepl_errors from Distribution..MSrepl_errors with (nolock)
 SELECT * INTO MSrepl_identity_range from Distribution..MSrepl_identity_range with (nolock)
 SELECT * INTO MSrepl_originators from Distribution..MSrepl_originators with (nolock)
-SELECT top 100 * INTO MSrepl_transactions_OLDEST from Distribution..MSrepl_transactions with (nolock)
-	order by xact_seqno asc
-SELECT top 100 * INTO MSrepl_transactions_NEWEST from Distribution..MSrepl_transactions with (nolock)
-	order by xact_seqno desc
 SELECT * INTO MSrepl_version from Distribution..MSrepl_version with (nolock)
 SELECT * INTO MSreplication_monitordata from Distribution..MSreplication_monitordata with (nolock)
 SELECT * INTO MSsnapshot_agents from Distribution..MSsnapshot_agents with (nolock)
